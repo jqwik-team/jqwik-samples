48f264c89473a085ec3c9754fc1554d7dd4c97eb
Johannes Link <business@johanneslink.net>
2020-04-01 12:19:10 +0200

    Added simple integration test to jqwik-spring-boot-gradle

--- jqwik-spring-boot-gradle/src/main/java/my/example/project/CalculatorApplication.java
+++ jqwik-spring-boot-gradle/src/main/java/my/example/project/CalculatorApplication.java
@@ -2,12 +2,19 @@ package my.example.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
+@RestController
 public class CalculatorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CalculatorApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "world";
 	}
 
 }
