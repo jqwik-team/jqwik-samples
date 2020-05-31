c285ffbfadef499224f2585cfb775baf681a4204
Nicolas Couvrat <nicolas.couvrat@sony.com>
2020-05-31 15:43:51 +0900

    Minimal junit 4 setup

---
+++ jqwik-multi-engine-maven/src/test/java/my/example/project/Junit4Tests.java
@@ -0,0 +1,12 @@
+package my.example.project;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class Junit4Tests {
+  @Test
+  public void test() {
+    assertTrue(true);
+  }
+}
