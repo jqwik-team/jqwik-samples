7923b3a078ab472caa966e08bdd662a2ccb5cc6b
Nicolas Couvrat <nicolas.couvrat@sony.com>
2020-05-31 15:36:27 +0900

    Minimal junit5 setup

---
+++ jqwik-multi-engine-maven/src/test/java/my/example/project/Junit5Tests.java
@@ -0,0 +1,12 @@
+package my.example.project;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class Junit5Tests {
+  @Test
+  void test() {
+    assertEquals(3, 2 + 1);
+  }
+}
