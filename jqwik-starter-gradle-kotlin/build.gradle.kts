b67c33fbabe36a12b86f0a757166959882638897
Johannes Link <business@johanneslink.net>
2024-06-22 10:46:11 +0200

    Upgrade jqwik-starter-gradle-kotlin to Kotlin 2

--- jqwik-starter-gradle-kotlin/build.gradle.kts
+++ jqwik-starter-gradle-kotlin/build.gradle.kts
@@ -1,24 +1,25 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.9.24"
+    kotlin("jvm") version "2.0.0"
 }
 
 repositories {
     mavenCentral()
     // Only necessary if you use jqwik's SNAPSHOT releases:
-    // maven(url = "https://oss.sonatype.org/content/repositories/snapshots")
+    maven(url = "https://s01.oss.sonatype.org/content/repositories/snapshots")
 }
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib")
 
-    testImplementation("net.jqwik:jqwik:1.8.5")
-    testImplementation("net.jqwik:jqwik-kotlin:1.8.5")
-    testImplementation("org.assertj:assertj-core:3.24.2")
+    testImplementation("net.jqwik:jqwik:1.9.0-SNAPSHOT")
+    testImplementation("net.jqwik:jqwik-kotlin:1.9.0-SNAPSHOT")
+    testImplementation("org.assertj:assertj-core:3.26.0")
 
     // Optional but recommended to get annotation related API warnings, e.g. for @CheckReturnValue
-    compileOnly("org.jetbrains:annotations:23.0.0")
+    compileOnly("org.jspecify:jspecify:0.3.0")
+    compileOnly("com.google.errorprone:error_prone_annotations:2.28.0")
 }
 
 tasks.withType<Test> {
@@ -32,12 +33,13 @@ tasks.withType<Test> {
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions {
+    compilerOptions {
         freeCompilerArgs = listOf(
-		  "-Xjsr305=strict", // Strict interpretation of nullability annotations in jqwik API
-		  "-Xemit-jvm-type-annotations" // Enable nnotations on type variables
-		)
-        jvmTarget = "17" // 1.8 or above
+            "-Xnullability-annotations=@org.jspecify.annotations:strict",
+            "-Xemit-jvm-type-annotations" // Enable annotations on type variables
+        )
+        apiVersion = org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_0
+        languageVersion = org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_0
         javaParameters = true // Get correct parameter names in jqwik reporting
     }
 }
